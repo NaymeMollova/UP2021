b83259400158ad2445aae791daec381cc0344bd3
NaymeMollova <64257736+NaymeMollova@users.noreply.github.com>
2021-11-23 10:18:44 +0200

    Create ex3.cpp

---
+++ week4/ex3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i <= 9)
+		{
+			cout << i << " ";
+		}
+	}
+
+	return 0;
+}
