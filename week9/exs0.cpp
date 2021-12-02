03b8ce579be692a5250b592927cb2391206393b2
NaymeMollova <64257736+NaymeMollova@users.noreply.github.com>
2021-12-02 13:09:57 +0200

    Create exs0.cpp

---
+++ week9/exs0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* num1, int* num2)
+{
+	*num1 = *num1 + *num2;
+	*num2 = *num1 - *num2;
+	*num1 = *num1 - *num2;
+	cout << *num1 << "   " << *num2;
+}
+
+int main()
+{
+	int num1, num2;
+	cin >> num1 >> num2;
+	swap(&num1, &num2);
+
+
+	return 0;
+}
