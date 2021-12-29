fb8e08bd42a33f4572c9720c7fcef97dc2753339
NaymeMollova <64257736+NaymeMollova@users.noreply.github.com>
2021-12-29 11:58:56 +0200

    Create exs3.cpp

---
+++ week11/exs3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int number)
+{
+	if (number == 1)
+	{
+		return 1;
+	}
+	if (number == 0)
+	{
+		return 1;
+	}
+	return number * factorial(number - 1);
+}
+
+int main()
+{
+	cout << factorial(0) << endl;
+	cout << factorial(1) << endl;
+	cout << factorial(5) << endl;
+	return 0;
+}
